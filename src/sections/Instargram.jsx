b9be263d8eb0622faffedecb54a1279c8e94027a
unknown <hseoglan@naver.com>
2026-02-26 07:39:49 +0900

    6차 업데이트

--- src/sections/Instargram.jsx
+++ src/sections/Instargram.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Instargram = () => {
   return (
-    <div>Instargram</div>
+    <div className='inner instar-inner'>
+      <div className="t-wrap">
+        <h2 className="tit"></h2>
+        <p className="txt"></p>
+      </div>
+      <ul className="instar-list">
+        <li>
+          <a href=""
+          // 배경 넣어보기
+          >1</a>
+        </li>
+      </ul>
+    </div>
   )
 }
 
--- src/sections/SkinCare.jsx
+++ src/sections/SkinCare.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
-
+import skincare from '../util/skincare'
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import './styles/SkinCare.scss'
 const SkinCare = () => {
   return (
-    <div>SkinCare</div>
+    <div className='inner skincare-inner'>
+      <div className="t-wrap">
+        <h2 className="tit">
+          Vegan Skincare
+        </h2>
+        <p className="txt">
+          피부에 건강한 영향력을 전달하고 꼭 필요한 성분만을 담아 <br />
+          놀라운 변화를 선사하는 비건 스킨케어 브랜드 토코보
+        </p>
+        <a href="#" className="btn">
+          meet tocobo
+        </a>
+      </div>
+      <div className="slider-wrap">
+        <div className="in">
+          <Swiper 
+          slidesPerView={2}
+          spaceBetween={30}
+          loop={true}
+          className="skincare-slider">
+            {skincare.map((sl)=>(
+
+            <SwiperSlide 
+            style={{backgroundImage:`url(${sl.image})`}}
+            key={sl.id} >
+              <a href={sl.link}>
+              {sl.id}
+              </a>
+            </SwiperSlide>
+            ))}
+
+          </Swiper>
+        </div>
+      </div>
+    </div>
   )
 }
 
