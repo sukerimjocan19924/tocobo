154f9615d110bd48f71de034d4f5fcf3f207999d
unknown <hseoglan@naver.com>
2026-02-26 04:06:19 +0900

    3차 업데이트

--- src/components/FixedTopBtn.jsx
+++ src/components/FixedTopBtn.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import './styles/FixedTopBtn.scss'
+import { useSmoothScroll } from '../hooks/useSmoothScroll'
 const FixedTopBtn = () => {
+    const scrollTo = useSmoothScroll()
     return (
         <div className='FixedTopBtn'>
-            <button>위로 올라가는 버튼</button>
-            <a href="">
+            <button
+            className='top-btn'
+                onClick={(e) => {
+                    e.preventDefault()
+                    scrollTo('hero')
+                }}
+
+            >위로 올라가는 버튼</button>
+            <a href="#" className='talk-btn'>
                 TCB
             </a>
             <p>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,62 @@
 import React from 'react'
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Pagination, Navigation } from 'swiper/modules';
+import './styles/Hero.scss'
+import {heroSlides} from '../util/hero'
 const Hero = () => {
   return (
-    <div className='Hero'>Hero</div>
+    <div className="hero-container">
+
+      <Swiper
+        modules={[Pagination, Navigation]}
+        pagination={{
+          el:'.swiper-pagination',
+          type: 'fraction'
+        }}
+        navigation={{
+          nextEl:'.arr-next',
+          prevEl:'.arr-prev'
+        }}
+        className="hero-slider"
+        loop={true}
+      >
+        {heroSlides.map((slide)=>(
+
+        <SwiperSlide 
+        className={`hero-slide ${slide.id}`}
+        style={{backgroundImage:`url(${slide.image.desktop})`}}
+        key={slide.id}>
+          <div className="inner">
+            <div className="t-wrap">
+              <h2 
+              className="tit"
+              dangerouslySetInnerHTML={{__html:slide.title}}
+               />
+                
+              <p className="txt">
+                {slide.subtitle}
+              </p>
+              <a href={slide.ctaHref} className='btn'>
+                {slide.ctaText}
+              </a>
+            </div>
+          </div>
+          
+          </SwiperSlide>
+        ))}
+
+
+      <div className="hero-nav">
+          <div className="arr-prev">prev</div>
+          <div className="swiper-pagination"></div>
+          <div className="arr-next">next</div>
+      </div>
+      </Swiper>
+    </div>
   )
 }
 
